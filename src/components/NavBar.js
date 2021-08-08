37e32bcc30ffd47c5bb899f091fba06698cae55e
Renda02 <rendaniluvhengo89@gmail.com>
2021-08-08 23:15:25 +0200

    styling  NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,43 +1,50 @@
 import React from "react";
-import arrow from "../images/icon-arrow-light.svg";
 import logo from "../images/logo.svg";
 import styled from "styled-components";
+import { RiArrowDropDownLine } from "react-icons/ri";
 
 function NavBar() {
   return (
     <NavBarWrapper>
       <Nav>
-          <NavLeft>
-        <img src={logo} alt="log" />
-        <ListWrap>
-          <NavListWrap>
-            Products <img src={arrow} alt="arrow" />
-          </NavListWrap>
-          <NavListWrap>
-            {" "}
-            Company <img src={arrow} alt="arrow" />
-          </NavListWrap>
-          <NavListWrap>
-            {" "}
-            Connect <img src={arrow} alt="arrow" />
-          </NavListWrap>
-        </ListWrap>
+        <NavLeft>
+          <img src={logo} alt="log" />
+          <ListWrap>
+            <NavListWrap>
+              Products{" "}
+              <NavIconSpan>
+                <RiArrowDropDownLine />
+              </NavIconSpan>
+            </NavListWrap>
+            <NavListWrap>
+              Company{" "}
+              <NavIconSpan>
+                <RiArrowDropDownLine />
+              </NavIconSpan>
+            </NavListWrap>
+            <NavListWrap>
+              Connect{" "}
+              <NavIconSpan>
+                <RiArrowDropDownLine />
+              </NavIconSpan>
+            </NavListWrap>
+          </ListWrap>
         </NavLeft>
-        <div>
-          <a href="/" alt="login">
+        <NavLinks>
+          <FirstLinkButton href="/" alt="login">
             Login
-          </a>
-          <a href="/">Signup</a>
-        </div>
+          </FirstLinkButton>
+          <SecondNavLink href="/">Signup</SecondNavLink>
+        </NavLinks>
       </Nav>
-      <div>
+      <ContentContainer>
         <h1>A modern publishing platform</h1>
         <p> Grow your audience and build your online brand</p>
-        <div className="main-link">
+        <MainLink>
           <a href="/"> Start for Free</a>
           <a href="/"> Learn More</a>
-        </div>
-      </div>
+        </MainLink>
+      </ContentContainer>
     </NavBarWrapper>
   );
 }
@@ -57,19 +64,68 @@ const Nav = styled.nav`
   justify-content: space-between;
 `;
 
-const NavLeft= styled.div`
-display:flex;
-justify-content:space-evenly;`;
+const SecondNavLink = styled.a`
+  text-decoration: none;
+  font-weight: 500;
+  letter-spacing: 1px;
+  font-size: 1.2em;
+  padding: 0.6em 1.2em;
+  color: hsl(0, 0%, 100%);
+  margin-right: 2em;
+  border: 1px solid hsl(0, 0%, 100%);
+  border-radius: 8px;
+`;
+
+const NavLeft = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`;
 
 const ListWrap = styled.ul`
   padding: 0;
   display: flex;
   list-style: none;
   font-weight: 400;
-  margin-left: 3em;
+  margin-left: 2em;
 `;
 
-const NavListWrap= styled.li`
-margin: 0 1em;
-cursor: pointer;
+const NavListWrap = styled.li`
+  margin: 0 1em;
+  cursor: pointer;
+  display: flex;
 `;
+
+const NavIconSpan = styled.span`
+  padding: 0.1em;
+`;
+
+const NavLinks = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const FirstLinkButton = styled.a`
+  text-decoration: none;
+  font-weight: 500;
+  letter-spacing: 1px;
+  font-size: 1.2em;
+  padding: 0.6em 1.2em;
+  color: hsl(0, 0%, 100%);
+  margin-right: 1em;
+
+  &:hover,
+  :focus {
+    cursor: pointer;
+    color: hsl(237, 17%, 21%);
+    border: 1px solid hsl(0, 0%, 100%);
+    border-radius: 8px;
+    background-color: hsl(0, 0%, 100%);
+  }
+`;
+
+
+const ContentContainer=styled.div`
+text-align:center;
+padding-bottom:6em;`;
+
+const MainLink=styled.div`
+margin:2em 0;`;
